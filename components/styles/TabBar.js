690314d1c41640affdcfbd081077922b3db76281
백지환 <jihwan3118@naver.com>
2024-01-17 20:46:07 +0900

    TRU-14
    1. 바로가기 아이콘에서 png 이미지로 변경
    2. 사용법 세션 추가
    3. 총 자산 이미지 추가, 왼쪽 바로가기 아이콘 png 이미지로 변경
    4. 하단 tab bar 이미지 링크 추가

--- components/styles/TabBar.js
+++ components/styles/TabBar.js
@@ -1,8 +1,8 @@
 import {
     View,
     TouchableOpacity,
+    Image
 } from 'react-native'
-import Icon from './Icons';
 import DarkMode from '../../components/styles/DarkMode'
 
 
@@ -12,7 +12,7 @@ const TabBar = ({ state, descriptors, navigation }) => {
             style={[
                 DarkMode.darkView,
                 {
-                    height: 90,
+                    height: 80,
                 }
             ]}
         >
@@ -22,8 +22,8 @@ const TabBar = ({ state, descriptors, navigation }) => {
                     width: '100%',
                     height: '100%',
                     backgroundColor: '#242424', // 원하는 색상으로 변경
-                    borderTopLeftRadius: 15,
-                    borderTopRightRadius: 15,
+                    borderTopLeftRadius: 10,
+                    borderTopRightRadius: 10,
                     borderWidth: 1,
                     borderColor: '#767676',
                     borderBottomWidth: 0,
@@ -50,10 +50,16 @@ const TabBar = ({ state, descriptors, navigation }) => {
                     // 아이콘 이름을 동적으로 가져오는 대신 직접 지정
                     let iconName;
                     if (route.name === 'Home') {
-                        iconName = 'home-outline';
-                    } else if (route.name === 'AssetsMoreColor') {
-                        iconName = 'person-outline';
-                    }
+                        iconName = require('../../assets/icons/home-grayline.png');
+                    } else if (route.name === 'Search') {
+                        iconName = require('../../assets/icons/search-grayline.png');
+                    } else if (route.name === 'Chat') {
+                        iconName = require('../../assets/icons/chat-grayline.png');
+                    } else if (route.name === 'Bell') {
+                        iconName = require('../../assets/icons/bell-grayline.png');
+                    } else if (route.name === 'Man') {
+                        iconName = require('../../assets/icons/man-grayline.png');
+                    } 
 
 
 
@@ -72,10 +78,8 @@ const TabBar = ({ state, descriptors, navigation }) => {
                                 key={index}
                                 onPress={onPress}
                             >
-                                <Icon
-                                    name={iconName} // options에 tabBarIconName을 추가하여 아이콘 이름을 동적으로 가져옴
-                                    size={24}
-                                    color={isFocused ? 'white' : '#767676'} // 선택된 탭 색상과 선택되지 않은 탭 색상을 다르게 지정
+                                <Image
+                                    source={iconName}
                                 />
                             </TouchableOpacity>
                         </View>
--- screens/Main.js
+++ screens/Main.js
@@ -5,6 +5,7 @@ import {
     TouchableOpacity,
     ScrollView,
     StyleSheet,
+    Image,
 } from 'react-native'
 import { useState } from 'react'
 
@@ -13,12 +14,13 @@ import DarkMode from '../components/styles/DarkMode'
 
 
 const Home = (props) => {
+    // 다크 모드
     const [ui, setUI] = useState(false);
 
     return (
         <SafeAreaView
             style={[
-                ui == false ? DarkMode.lightView : DarkMode.darkView,
+                ui != false ? DarkMode.lightView : DarkMode.darkView,
                 {
                     flex: 1,
                 }
@@ -30,7 +32,7 @@ const Home = (props) => {
                     {/* 사용자 세션 */}
                     <View style={styles.userSection}>
                         <Text style={[
-                            ui == false ? DarkMode.lightMainText : DarkMode.darkMainText,
+                            ui != false ? DarkMode.lightMainText : DarkMode.darkMainText,
                             styles.userText,
                         ]}>
                             백지환
@@ -44,26 +46,44 @@ const Home = (props) => {
                                 ui == true ? setUI(false) : setUI(true)
                             }}
                         >
-                            <Text style={styles.userSubText}>
-                                내 자산 바로가기 ᐳ
-                            </Text>
+                            <View style={styles.shortCut}>
+                                <Text style={styles.userSubText}>
+                                    내 자산 바로가기
+                                </Text>
+                                <Image
+                                    style={styles.shortcutIcon}
+                                    source={require('../assets/icons/ShortCut-white.png')}
+                                />
+                            </View>
+
+
                         </TouchableOpacity>
                     </View>
 
                     {/* 사용법 세션 */}
-                    <View 
+                    <View
                         style={[
-                            ui == false ? DarkMode.lightSubView : DarkMode.darkSubView,
+                            ui != false ? DarkMode.lightSubView : DarkMode.darkSubView,
                             styles.section, { height: 90 }
                         ]}
                     >
+                        <View style={styles.explanation}>
+                            <Text style={styles.explanationSubText}>
+                                실물 자산을 편하게 관리하는 방법!
+                            </Text>
+                            <Text style={styles.explanationMainText}>SODA 사용 방법을 알려드립니다</Text>
+                        </View>
+                        <Image
+                            source={require('../assets/icons/illustration.png')}
+                            style={styles.illustration}
+                        />
 
                     </View>
 
                     {/* 총 자산 세션 */}
                     <TouchableOpacity
                         style={[
-                            ui == false ? DarkMode.lightSubView : DarkMode.darkSubView,
+                            ui != false ? DarkMode.lightSubView : DarkMode.darkSubView,
                             styles.section,
                             {
                                 height: 95,
@@ -72,26 +92,36 @@ const Home = (props) => {
                             }
                         ]}
                     >
-                        <View style={styles.totalImage}></View>
+                        <View style={styles.assetsContainer}>
+                            <View style={styles.totalImage}></View>
+                            <Image
+                                source={require('../assets/icons/Hand-Icon.png')}
+                                style={styles.totalAssetsImage}
+                            />
+                        </View>
+
                         <Text style={styles.totalSubText1}>
                             총 자산{'\n'}
                             <Text
                                 style={[
-                                    ui == false ? DarkMode.lightMainText : DarkMode.darkMainText,
+                                    ui != false ? DarkMode.lightMainText : DarkMode.darkMainText,
                                     styles.totalMainText
                                 ]}
                             >
                                 1,301,590,000원
                             </Text>
                         </Text>
-                        <Text style={styles.totalSubText2}>ᐳ</Text>
+                        <Image
+                            style={styles.totalShortcutIcon}
+                            source={require('../assets/icons/ShortCut-white.png')}
+                        />
                     </TouchableOpacity>
 
 
                     {/* 자산 그래프 세션 */}
-                    <View 
+                    <View
                         style={[
-                            ui == false ? DarkMode.lightSubView : DarkMode.darkSubView,
+                            ui != false ? DarkMode.lightSubView : DarkMode.darkSubView,
                             styles.section, { height: 280 }
                         ]}
                     >
@@ -99,9 +129,9 @@ const Home = (props) => {
                     </View>
 
                     {/* 자산 비율 세션 */}
-                    <View 
+                    <View
                         style={[
-                            ui == false ? DarkMode.lightSubView : DarkMode.darkSubView,
+                            ui != false ? DarkMode.lightSubView : DarkMode.darkSubView,
                             styles.section, { height: 309 }
                         ]}
                     >
@@ -120,36 +150,62 @@ export default Home
 
 const styles = StyleSheet.create({
     section: {
-        width: '94%',
+        width: '91%',
         borderRadius: 20,
         marginBottom: 20,
     },
 
-
     //사용자 세션
     userSection: {
         flexDirection: 'row',
-        width: '94%',
+        width: '91%',
         height: 70,
         alignItems: 'center',
     },
 
     userText: {
         fontSize: 20,
-        fontWeight: 700,
+        fontWeight: 'bold',
     },
     userSubText: {
         fontSize: 14,
-        fontWeight: 700,
-        color: '#767676',
+        fontWeight: 'bold',
+        color: '#DBDBDB',
+        marginRight: 8,
+    },
+
+    shortCut: { //바로가기 버튼
+        flexDirection: 'row',
+    },
+    shortcutIcon: { // 바로가기 아이콘
+        marginTop: 1,
     },
 
 
-
-
     //사용법 세션
+    explanation: {
 
-
+    },
+    explanationSubText: {
+        position: 'absolute',
+        color: '#767676',
+        fontWeight: 'normal',
+        fontSize: 14,
+        left: 20,
+        top: 20,
+    },
+    explanationMainText: {
+        position: 'absolute',
+        fontSize: 18,
+        left: 20,
+        color: '#DBDBDB',
+        top: 49
+    },
+    illustration: {
+        position: 'absolute',
+        bottom: 0,
+        right: 40
+    },
 
 
     //총 자산 세션
@@ -158,21 +214,32 @@ const styles = StyleSheet.create({
         borderRadius: 100,
         width: 50,
         height: 50,
-        margin: 20,
+        marginLeft: 20,
+        marginRight: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    totalAssetsImage: {
+        alignSelf: 'center',
+        position: 'absolute',
+        marginLeft: 30.
     },
     totalSubText1: {
-        color: '#767676',
+        color: '#DBDBDB',
         fontSize: 18,
-        fontWeight: 500,
+        fontWeight: 'normal',
     },
     totalMainText: {
-        fontWeight: 700,
+        fontWeight: 'bold',
         lineHeight: 25,
     },
-    totalSubText2: {
-        color: '#767676',
-        fontSize: 18,
-        fontWeight: 'bold',
+
+    assetsContainer: {
+        flexDirection: 'row'
+    },
+    
+    totalShortcutIcon: {   // 총 자산 바로가기 아이콘 스타일
+        marginTop: 1,
         position: 'absolute',
         right: 20,
     },
